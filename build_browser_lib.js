0e3276ee588c16cfc2d21832d56cf2143da37639
Joshua Tauberer <jt@occams.info>
2014-01-12 14:01:16 -0500

    build the browser library with more reliable library dependency detection

--- build_browser_lib.js
+++ build_browser_lib.js
@@ -1,30 +1,35 @@
 var fs = require('fs');
 
-var libs = {
-	"deep-equal": "node_modules/deep-equal/index.js",
-	"base.js": "jot/base.js",
-	"values.js": "jot/values.js",
-	"sequences.js": "jot/sequences.js",
-	"objects.js": "jot/objects.js",
-	"collab.js": "jot/collab.js",
-};
 
 process.stdout.write("var jot_modules = { }\n");
 
 process.stdout.write(fs.readFileSync("jot/browser_platform.js"));
 
-for (var name in libs) {
-	process.stdout.write("jot_modules['" + name + "'] =  (function(module) {\n");
+add_lib("./jot/values.js");
+add_lib("./jot/sequences.js");
+add_lib("./jot/objects.js");
+add_lib("./jot/collab.js");
+
+function add_lib(module_name) {
+	// how does 'require' know this module?
+	var filename = require.resolve(module_name);
+
+	process.stdout.write("// " + filename + "\n");
+
+	// handle dependencies
+	require(module_name);
+	for (var child in require.cache[filename].children) {
+		add_lib(require.cache[filename].children[child].filename);
+	}
+
+	process.stdout.write("jot_modules['" + filename + "'] =  (function(module) {\n");
 	process.stdout.write("module.exports = { };\n");
 	process.stdout.write("var exports = module.exports;\n");
 	process.stdout.write("var __dirname = 'jot';\n");
-	var body = fs.readFileSync(libs[name]);
+	var body = fs.readFileSync(filename);
 	process.stdout.write(body);
 	process.stdout.write("return module;");
 	process.stdout.write("}( {} ));\n");
 	process.stdout.write("\n");
 }
-
-process.stdout.write(fs.readFileSync(__dirname+'/node_modules/googlediff/javascript/diff_match_patch_uncompressed.js'));
-process.stdout.write("jot_modules['googlediff'] = { exports: diff_match_patch };\n");
 
--- jot/browser_platform.js
+++ jot/browser_platform.js
@@ -1,9 +1,14 @@
 /* Utility code for running the library within a browser. */
 
 function require(module_name) {
-	module_name = module_name.replace(/^(\.\/)?jot\//, "");
-	if (module_name in jot_modules)
-		return jot_modules[module_name].exports;
+	function endswith(string, suffix) {
+	    return string.indexOf(suffix, string.length - suffix.length) !== -1;
+	};
+	if (module_name.indexOf("./") == 0) module_name = module_name.substring(2);
+	for (var filename in jot_modules) {
+		if (endswith(filename, "/" + module_name) || endswith(filename, "/" + module_name + ".js") || endswith(filename, module_name + "/index.js"))
+			return jot_modules[filename].exports;
+	}
 	throw module_name + " not available!";
 }
 
