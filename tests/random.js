4e158144707f1fdda8c5f055be9702a41b050e01
Joshua Tauberer <jt@occams.info>
2018-06-02 07:12:41 -0400

    break out random tests into subtests

--- tests/random.js
+++ tests/random.js
@@ -1,14 +1,22 @@
 var test = require('tap').test;
 var jot = require('../jot')
 
-test('random', function(t) {
-	for (var i = 0; i < 1000; i++) {
+test('random', function(tt) {
+for (var i = 0; i < 1000; i++) {
 		// Start with a random initial document.
 		var initial_value = jot.createRandomValue();
 		
 		// Create two operations on the initial document.
 		var op1 = jot.createRandomOpSequence(initial_value, Math.floor(10*Math.random())+1)
 		var op2 = jot.createRandomOpSequence(initial_value, Math.floor(10*Math.random())+1)
+
+		tt.test(
+			JSON.stringify({
+				"value": initial_value,
+				"op1": op1.toJSON(),
+				"op2": op2.toJSON(),
+			}),
+			function(t) {
 
 		// Apply each to get the intermediate values.
 		var val1 = op1.apply(initial_value);
@@ -26,7 +34,10 @@ test('random', function(t) {
 
 		// Check that we can compute a diff.
 		var d = jot.diff(initial_value, val1b);
-	}
 
-    t.end();
+	    t.end();
+
+		});
+}
+tt.end();
 });
