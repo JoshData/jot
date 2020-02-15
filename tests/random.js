ed860b782b3cf9aa0aa796f1d4a4d836a25ec91e
Joshua Tauberer <jt@occams.info>
2020-02-15 07:59:17 -0500

    handle floating point precision issues in the random operation tests

--- tests/random.js
+++ tests/random.js
@@ -1,6 +1,18 @@
 var test = require('tap').test;
 var jot = require('../jot')
 
+function assertEqual(t, v1, v2) {
+	// After applying math operators, floating point
+	// results can come out a little different. Round
+	// numbers to about as much precision as floating
+	// point numbers have anyway.
+	if (typeof v1 == "number" && typeof v2 == "number") {
+		v1 = v1.toPrecision(8);
+		v2 = v2.toPrecision(8);
+	}
+	t.deepEqual(v1, v2);
+}
+
 test('random', function(tt) {
 for (var i = 0; i < 1000; i++) {
 		// Start with a random initial document.
@@ -25,12 +37,12 @@ for (var i = 0; i < 1000; i++) {
 		// Check that the parallel rebases match.
 		var val1b = op2.rebase(op1, { document: initial_value }).apply(val1);
 		var val2b = op1.rebase(op2, { document: initial_value }).apply(val2);
-		t.deepEqual(val1b, val2b);
+		assertEqual(t, val1b, val2b);
 
 		// Check that they also match using composition.
 		var val1c = op1.compose(op2.rebase(op1, { document: initial_value })).apply(initial_value);
 		var val2c = op2.compose(op1.rebase(op2, { document: initial_value })).apply(initial_value);
-		t.deepEqual(val1c, val2c);
+		assertEqual(t, val1c, val2c);
 
 		// Check that we can compute a diff.
 		var d = jot.diff(initial_value, val1b);
